46e0004b0fd2776aff1a1ea44ce80fa87723b0c3
Emanuel <lejiei@gmail.com>
2023-03-17 10:53:44 -0400

    add delete, filter

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,64 +1,98 @@
-import React, {FC, ChangeEvent, useState} from 'react';
-import './App.css';
-import TodoTask from './Components/TodoTask';
-import { ITask } from './Interfaces';
+import userEvent from "@testing-library/user-event";
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import TodoTask from "./Components/TodoTask";
+import { ITask } from "./Interfaces";
 
-const App: FC = ()=> {
+import { filter } from 'rxjs/operators';
 
-  const [task, setTask] = useState<string>("");
-  const [deadline, setDeadLine] = useState<number>(0);
-  const [todoList, setTodoList] = useState<ITask[]>([]);
+const App: FC = () => {
+  const [filter, setFilter] = useState<string>("");
+  const [task, setTask] = useState<string>(""); 
+  const [deadline, setDeadLine] = useState<string>();
+  const [todoList, setTodoList] = useState<{taskName: string; deadline: string | undefined} []>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {
       setTask(event.target.value);
     } else {
-      setDeadLine(Number(event.target.value));
+      setDeadLine((event.target.value));
     }
   };
-
-
+ 
+  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setDeadLine((event.target.value));
+  };
+  
   const addTask = (): void => {
     const newTask = { taskName: task, deadline: deadline };
-    setTodoList([...todoList, newTask]);
-    setTask("");
-    setDeadLine(0);
+    setTodoList([...todoList, newTask]);  
   };
 
-  const completeTask =(taskNameToDelete: string): void =>{
-    setTodoList(todoList.filter((task)=> {
-      return task.taskName != taskNameToDelete;
-    }))
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
+  const handleOnClick =() =>{
+    const findTask = todoList?.length > 0 ? todoList?.filter(f => f.taskName === filter):undefined;
+    const newTask = { taskName: task, deadline: deadline };
+    console.log("resultados", findTask);
+    setTodoList([...todoList, newTask]);  
   }
 
   return (
-    <div className='App'>
-      <div className='header'></div>
-      <div className='inputContainer'>      
-      <input
-            type="text"
-            placeholder="Task..."
-            name="task"
-            value={task}
-            onChange={handleChange}
-          />
-      <input
+    <div className="App">
+      <div className="header"></div>
+      
+      <div className="inputContainer">
+        <input
+          type="text"
+          placeholder="filter..."         
+          value={filter}          
+        />        
+
+        <button onClick={handleOnClick}>Filter</button>
+      </div>
+      
+      <div className="inputContainer">
+        <input
+          type="text"
+          placeholder="Task..."
+          name="task"
+          value={task}
+          onChange={handleChange}
+        />
+        <select onChange={selectChange}>
+        <option selected disabled>
+          Choose one
+        </option>
+        <option value="completed">Completed</option>
+        <option value="nocompleted">No Completed</option>       
+        </select>
+        {/* <input
             type="number"
             placeholder="Deadline (in Days)..."
             name="deadline"
             value={deadline}
             onChange={handleChange}
-          />
+          /> */}
 
-      <button onClick={addTask}>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
-      <div className='todoList'>
-        {todoList.map((task: ITask, key: number)=>{
-          return <TodoTask key={key} task={task} completeTask={completeTask}/>;
+
+
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })}
       </div>
     </div>
   );
-}
+};
 
 export default App;
